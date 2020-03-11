e8c8171168bf96d82e62bd9aebae81e233461f45
Sean Davis <seandavi@gmail.com>
2020-03-11 17:05:48 -0400

    small changes to generate script

--- generate.sh
+++ generate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 for LANG in python go 
 do
+rm -rf $LANG-client
 docker run --rm -v ${PWD}:/local \
   openapitools/openapi-generator-cli generate \
   -i https://api.omicidx.cancerdatasci.org/openapi.json \
-  -g $LANG -o /local/$LANG
+  --remove-operation-id-prefix \
+  --package-name omicidx_client \
+  -g $LANG -o /local/$LANG-client
 done
 
 find . -name git_push.sh -exec rm {} \;
